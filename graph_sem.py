603b2239a7ad57252534e8a1c9495e08affce849
opatejdl <140671894+Patik04@users.noreply.github.com>
2024-11-06 17:59:28 +0100

    Add files via upload
    Python file which shows graph of humidity and temperature

---
+++ graph_sem.py
@@ -0,0 +1,84 @@
+import paho.mqtt.client as mqtt
+import json
+import matplotlib.pyplot as plt
+
+time_hum = []
+hum = []
+temp = []
+time_temp = []
+
+BROKER = "test.mosquitto.org"
+TOP_TEMP = "zpi/temperature"
+TOP_HUM = "zpi/humidity"
+
+
+def on_message(client, userdata, msg):
+    data = json.loads(msg.payload.decode("utf-8"))
+
+    if (msg.topic == TOP_HUM):
+        if (len(hum) >= 10):
+            hum.pop(0)
+            time_hum.pop(0)
+
+        timeVal = data["time"].replace(" ", "\n")
+        time_hum.append(timeVal)
+        hum.append(data["humidity"])
+
+    elif (msg.topic == TOP_TEMP):
+        if (len(temp) >= 10):
+            temp.pop(0)
+            time_temp.pop(0)
+
+        timeVal = data["time"].replace(" ", "\n")
+        time_temp.append(timeVal)
+        temp.append(data["temperature"])
+    print("Recieved: ", data)
+
+
+if __name__ == "__main__":
+    client = mqtt.Client()
+
+    client.on_message = on_message
+    client.connect(BROKER, 1883)
+    client.subscribe(TOP_HUM)
+    client.subscribe(TOP_TEMP)
+
+    client.loop_start()
+    try:
+        while (True):
+            if (len(time_hum) != 0 and len(hum) != 0
+                    and len(time_temp) != 0 and len(temp) != 0):
+                plt.clf()
+
+                # Humudity graph
+                plt.subplot(1, 2, 1)
+                plt.plot(time_hum, hum, marker="o")
+
+                plt.xlabel("Time Value")
+                plt.ylabel("Humidity Value(%)")
+                plt.title("Humidity Graph")
+                plt.xticks(rotation=45)
+                plt.grid()
+
+                # Temperature graph
+                plt.subplot(1, 2, 2)
+                plt.plot(time_temp, temp, marker="o")
+
+                plt.xlabel("Time Value")
+                plt.ylabel("Temperature Value(°C)")
+                plt.title("Temperature Graph")
+                plt.xticks(rotation=45)
+                plt.grid()
+
+                # show plot
+                plt.tight_layout()
+                plt.draw()
+                plt.pause(1)
+
+            else:
+                plt.clf()
+                plt.title("Waiting for data....")
+                plt.pause(1)
+    finally:
+        client.loop_stop()
+        client.disconnect()
